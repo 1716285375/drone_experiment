009c667706c23953a51e9954514a78933d0d24db
1716285375 <1716285375@qq.com>
2024-06-24 19:17:48 +0800

    fix the fly bug in the drone' episode

--- airgym/envs/drone_env.py
+++ airgym/envs/drone_env.py
@@ -16,7 +16,9 @@ from experiment.airgym.envs.airsim_env import AirSimEnv
 class AirSimDroneEnv(AirSimEnv):
     def __init__(self, ip_address, step_length, image_shape):
         super().__init__(image_shape)
+        self.ip_address = ip_address
         self.step_length = step_length  # 无人机执行动作时的偏移步长
+        self.steps = 0
         self.image_shape = image_shape
 
         self.img_dir = os.path.join(os.getcwd(), 'data', 'images')    # 图片存储目录
@@ -26,17 +28,23 @@ class AirSimDroneEnv(AirSimEnv):
             "collision": False,
             "prev_position": np.zeros(3),
         }
-        self.takeoff_height = -1  # 设定的飞行高度，例如10米
+        self.takeoff_height = -5  # 设定的飞行高度，例如10米
 
-        self.goal = np.array([18, 19, -1])
+        # self.goal = np.array([np.random.randint(300, 601), np.random.randint(300, 601), -1])
+        self.goal = np.array([33, -34, -5])
 
-        self.drone = airsim.MultirotorClient(ip=ip_address)  # 加载无人机
+        self.drone = airsim.MultirotorClient(ip=self.ip_address)  # 加载无人机
         self.action_space = spaces.Discrete(5)  # 动作空间：5种动作
-        self._setup_flight()    # 初始化无人机状态
+        # self._setup_flight()    # 初始化无人机状态
 
-        self.image_request = airsim.ImageRequest(
-            3, airsim.ImageType.DepthPerspective, True, False
-        )
+        self.image_request = [
+            # png format
+            airsim.ImageRequest(0, airsim.ImageType.Scene),
+            # uncompressed RGB array bytes
+            airsim.ImageRequest(1, airsim.ImageType.Scene, False, False),
+            # floating point uncompressed image
+            airsim.ImageRequest(1, airsim.ImageType.DepthPlanar, True)
+        ]
 
     def __del__(self):
         pass
@@ -48,24 +56,34 @@ class AirSimDroneEnv(AirSimEnv):
 
         # Set home position and velocity
         self.drone.moveToZAsync(self.takeoff_height, 3).join()   # 移动到指定的位置：（x, y, z），速度为10m/s
-        self.drone.moveByVelocityAsync(1, -0.6, 0, 1).join()    # 以指定x, y, z方向的速度移动，持续时间：5s
+        self.drone.moveByVelocityAsync(1, -0.6, 0, 5).join()    # 以指定x, y, z方向的速度移动，持续时间：5s
 
     def transform_obs(self, responses):
         """处理无人机获取的图片"""
-        img1d = np.array(responses[0].image_data_float, dtype=np.float32)
-        img1d = 255 / np.maximum(np.ones(img1d.size), img1d)
-        img2d = np.reshape(img1d, (responses[0].height, responses[0].width))
+        img1d = np.array(responses[2].image_data_float, dtype=np.float32)
+
+        # img1d = 255 / np.maximum(np.ones(img1d.size), img1d)
+        img2d = np.reshape(img1d, (responses[2].height, responses[2].width))
+
+        # img1d = np.frombuffer(responses[1].image_data_uint8, dtype=np.uint8)
+        # img_rgb = img1d.reshape(responses[1].height, responses[1].width, 3)
+        # write to png
+        # filename = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime(time.time()))
+        # airsim.write_png(os.path.join(self.img_dir, filename + '.png'), img_rgb)
 
         from PIL import Image
 
+        # image = Image.fromarray(img2d)
         image = Image.fromarray(img2d)
-        im_final = np.array(image.resize((84, 84)).convert("L"))
+        im_final = np.array(image.resize((55, 100)))
 
-        return im_final.reshape([1, 84, 84])
+        return im_final.reshape([1, 55, 100])
 
     def _get_obs(self):
-        responses = self.drone.simGetImages([self.image_request])
+        responses = self.drone.simGetImages(self.image_request)
+        # print(responses)  # debug
         image = self.transform_obs(responses)
+        # print(image.shape)
         self.drone_state = self.drone.getMultirotorState()
 
         self.state["prev_position"] = self.state["position"]
@@ -89,11 +107,11 @@ class AirSimDroneEnv(AirSimEnv):
             quad_vel.x_val + quad_offset[0],
             quad_vel.y_val + quad_offset[1],
             0,
-            3).join()
+            5).join()
 
     def _compute_reward(self):
         """动作执行后所获得的计算奖励"""
-        quad_pt = np.array(
+        quad_pt_curr = np.array(
             list(
                 (
                     self.state["position"].x_val,
@@ -102,27 +120,44 @@ class AirSimDroneEnv(AirSimEnv):
                 )
             )
         )
-        dist = np.sqrt(np.power((self.goal[0] - quad_pt[0]), 2) +
-                       np.power((self.goal[1] - quad_pt[1]), 2) +
-                       np.power((self.goal[2] - quad_pt[2]), 2)
-                       )
 
+        quad_pt_prev = np.array(
+            list(
+                (
+                    self.state["prev_position"].x_val,
+                    self.state["prev_position"].y_val,
+                    self.state["prev_position"].z_val,
+                )
+            )
+        )
+        dist_curr = np.sqrt(np.power((self.goal[0] - quad_pt_curr[0]), 2) +
+                            np.power((self.goal[1] - quad_pt_curr[1]), 2) +
+                            np.power((self.goal[2] - quad_pt_curr[2]), 2))
+
+        dist_prev = np.sqrt(np.power((self.goal[0] - quad_pt_prev[0]), 2) +
+                            np.power((self.goal[1] - quad_pt_prev[1]), 2) +
+                            np.power((self.goal[2] - quad_pt_prev[2]), 2))
         done = 0
-        if self.state["collision"] or dist > 80:
-            reward = -1
-            if self.state["collision"]:
-                done = 1
-        elif 40 < dist < 80:
-            reward = -dist / 40 + 1
-        elif 1 < dist < 40:
-            reward = pow(0.5, 0.15 * dist)
+        if self.state["collision"]:
+            reward = -100
         else:
-            reward = 1
+            if self.steps >= 30:
+                reward = -100
+                self.steps = 0
+                # self.goal = np.array([np.random.randint(300, 601), np.random.randint(300, 601), -1])
+                done = 1
+            else:
+                if dist_curr < 7:
+                    reward = 100
+                    done = 1
+                else:
+                    reward = dist_prev - dist_curr
 
         return reward, done
 
     def step(self, action):
         self._do_action(action)
+        self.steps += 1     # 当前回合次数加1
         obs = self._get_obs()
         reward, done = self._compute_reward()
 
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 import gym
 from gym.envs.registration import register
@@ -9,6 +10,8 @@ import airsim
 import torch
 
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+
 
 import warnings
 # 屏蔽UserWarning警告
@@ -26,34 +29,38 @@ register(
     entry_point='__main__:AirSimDroneEnv',
     kwargs={
         'ip_address': '127.0.0.1',
-        'step_length': 0.25,
+        'step_length': 0.5,
         'image_shape': (144, 256, 1)}
 )
 
 BATCH_SIZE = 32        # 每次训练批次大小
 MAX_CAPACITY = 1000    # 经验回放区最大容量
-MINIMAL_SIZE = 4        #
+
 INPUT_CHANNELS = 1      # 输入的图像通道数
 NUM_ACTIONS = 5         # 动作数量
 
-learning_rate = 0.001
 epsilon = 0.1
+epsilon_end = 0.01
+epsilon_start = 0.90
+epsilon_decay = 1000
+frame_index = 1
+
+learning_rate = 0.001
+
 GAMMA = 0.99            # 时间折扣率
 
-NUM_EPISODES = 6000     # 最大尝试次数
+NUM_EPISODES = 500     # 最大尝试次数
+
 
 count = 0
-target_update = 10
+target_update = 100
 
+MINIMAL_SIZE = 4        #
 
-
-
-replay_buffer = ReplayBuffer(INPUT_CHANNELS, )
+replay_buffer = ReplayBuffer(MAX_CAPACITY)
 
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device(
     "cpu")
-
-
 
 
 if __name__ == "__main__":
@@ -61,8 +68,8 @@ if __name__ == "__main__":
     # env = gym.make('AirSimEnv-v0')
     # check_env(env)
 
-    # model = CnnDQN(env.observation_space.shape, env.action_space.n)
-    # target_model = CnnDQN(env.observation_space.shape, env.action_space.n)
+    # model = CNN(env.observation_space.shape, env.action_space.n)
+    # target_model = CNN(env.observation_space.shape, env.action_space.n)
     # memory = Memory(1000)
     # dqn_learn(env, model, target_model, memory, device=DEVICE)
 
@@ -97,16 +104,16 @@ if __name__ == "__main__":
     #                         '%.3f' % np.mean(return_list[-10:])
     #                 })
     #             pbar.update(1)
-    model = CnnDQN(INPUT_CHANNELS, NUM_ACTIONS)
+    model = CNN(INPUT_CHANNELS, NUM_ACTIONS)
     # 加载模型参数
-    model.load_state_dict(torch.load("./result/trained_mod/mod-rl-v0.pth"))
+    # model.load_state_dict(torch.load("./result/trained_mod/mod-rl-v0.pth"))
 
-    target_model = CnnDQN(INPUT_CHANNELS, NUM_ACTIONS)
+    target_model = CNN(INPUT_CHANNELS, NUM_ACTIONS)
     model.to(DEVICE)
     target_model.to(DEVICE)
 
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    criterion = nn.HuberLoss()
+    criterion = nn.MSELoss()
 
     env = gym.make('AirSimEnv-v0')
     return_list = []
@@ -118,32 +125,36 @@ if __name__ == "__main__":
         episode_return = 0
         print("**Episode: {}**".format(episode,))
         while not done:
+            epsilon = epsilon_end + (epsilon_start - epsilon_end) * math.exp(-1. * frame_index / epsilon_decay)
+            frame_index += 1
             if np.random.random() < epsilon:
                 action = env.action_space.sample()
             else:
-                action = model(torch.tensor(obs, dtype=torch.float32).to(DEVICE)).argmax().item()
+                state = torch.tensor(obs, dtype=torch.float32).unsqueeze(0)
+                # print(state.shape)
+                action = model(state.to(DEVICE)).argmax().item()
             next_obs, reward, done, _ = env.step(action)
-            print("Action: {} | Reward: {} | done: {}".format(episode, action, reward, done))
+            print("Action: {} | Reward: {} | done: {}".format(action, reward, done))
 
             replay_buffer.push(obs, action, reward, next_obs, done)
             obs = next_obs
 
             if len(replay_buffer) > BATCH_SIZE:
                 # update_q_function()
-                batch = replay_buffer .sample(BATCH_SIZE)
-                states, actions, rewards, next_states, dones = zip(*batch)
+                batch = replay_buffer.sample(BATCH_SIZE)
+                states, actions, rewards, next_states, dones = batch
 
                 states = torch.tensor(states, dtype=torch.float32).to(DEVICE)
-                actions = torch.tensor(actions, dtype=torch.int64).unsqueeze(1).to(DEVICE)
-                rewards = torch.tensor(rewards, dtype=torch.float32).unsqueeze(1).to(DEVICE)
+                actions = torch.tensor(actions, dtype=torch.int64).view(-1, 1).to(DEVICE)
+                rewards = torch.tensor(rewards, dtype=torch.float32).view(-1, 1).to(DEVICE)
                 next_states = torch.tensor(next_states, dtype=torch.float32).to(DEVICE)
-                dones = torch.tensor(dones, dtype=torch.float32).unsqueeze(1).to(DEVICE)
+                dones = torch.tensor(dones, dtype=torch.float32).view(-1, 1).to(DEVICE)
 
-                q_values = model(states).gather(1, actions)
-                max_next_q_values = target_model(next_states).max(1)[0].detach()
+                q_values = model(states).gather(1, actions)     # 按照gather中的张量元素的大小顺序重新排列
+                max_next_q_values = target_model(next_states).max(1)[0].detach()    # max(1)返回最大索引，max(1)[0]返回最大索引对应的值
                 target_q_values = rewards + GAMMA * max_next_q_values * (1 - dones)
 
-                loss = criterion(q_values, target_q_values)
+                loss = criterion(target_q_values, q_values)
 
                 optimizer.zero_grad()
                 loss.backward()
@@ -156,9 +167,13 @@ if __name__ == "__main__":
 
             episode_return += reward
 
-            env.render()
+            # env.render()
         print("return: {}".format(episode_return))
         print("------------------------------------------------------")
         return_list.append(episode_return)
+        if episode > 0 and episode % MINIMAL_SIZE/10 == 0:
+            x_ = np.arange(len(return_list))
+            plt.plot(x_, return_list)
+            plt.show()
     env.close()
     save_model(target_model.state_dict(), "./result/trained_mod")
